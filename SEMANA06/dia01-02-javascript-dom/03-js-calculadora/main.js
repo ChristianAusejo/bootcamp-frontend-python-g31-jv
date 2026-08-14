0afabe3514a21593ae3c34d0e8824c2afe394b9e
Christian Ausejo <christiangnr00@gmail.com>
2026-08-13 23:04:38 -0500

    calculadora-js

--- SEMANA06/dia01-02-javascript-dom/03-js-calculadora/main.js
+++ SEMANA06/dia01-02-javascript-dom/03-js-calculadora/main.js
@@ -11,4 +11,29 @@ const buttons = document.querySelectorAll('.button')
 
 // EVENTOS
 
+buttons.forEach(function(button) {
+    button.addEventListener('click' , function(event) {
+        //console.log('Hice click!', event.target)
+
+        const buttonText = event.target.textContent
+
+        if ('*-+'.includes(buttonText)) {
+            // 02. Accionamos el operador
+            operador = buttonText
+            operando = Number(numeroActual)
+            numeroActual = '0'
+        } else if (buttonText === '=') {
+            // TODO: Calcular las operaciones en base al número actual y operando
+        } else if (buttonText === 'C') {
+            // TODO: Limpiar el operando, operador y el input
+        } else {
+         // 01. Se presionó algún número
+            numeroActual = Number(numeroActual + buttonText)
+        }
+
+    
+
+       inputDisplay.value = numeroActual
+    })
+})
 

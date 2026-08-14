1212567aae889ad7855fff67bd1afc9a6921df47
Christian Ausejo <christiangnr00@gmail.com>
2026-08-13 22:38:56 -0500

    semana06-dia01-02-javascript-dom-03-js-calculadora

---
+++ SEMANA06/dia01-02-javascript-dom/03-js-calculadora/main.js
@@ -0,0 +1,14 @@
+let numeroActual = '0'
+let operador = ''
+let operando = ''
+
+// Consultar un elemento a la vez
+const inputDisplay = document.querySelector('#inputDisplay')  // querySelector es equivalente al getByID
+//console.log({inputDisplay})
+// Consultar multiples elementos a la vez
+const buttons = document.querySelectorAll('.button')
+//console.log({buttons})
+
+// EVENTOS
+
+
